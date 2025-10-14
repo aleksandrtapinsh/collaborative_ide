3bc47e56d2e6299e0fe6d7af3d461098e35a8733
ConnorSpringer <connorspringer6211@gmail.com>
2025-10-14 10:59:45 -0400

    fixed indentation

--- frontend/js/validationlogin.js
+++ frontend/js/validationlogin.js
@@ -2,13 +2,13 @@ function validate() {
     const signup = document.forms.signup
     const {name, email, password1, password2} = signup
 
-         if (email.value === "") {
-            alert("Please enter a valid Email.")
-            return false
-        }
-        else if (password.value === "") {
-            alert("Please enter a Password.")
-            return false
-        }
+    if (email.value === "") {
+        alert("Please enter a valid Email.")
         return false
+    }
+    else if (password.value === "") {
+        alert("Please enter a Password.")
+        return false
+    }
+    return false
 }
