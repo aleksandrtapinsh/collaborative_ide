748b353fa3d79400e09d697344e8ba931eeac230
Aleksandr Tapinsh <squrtle2003@gmail.com>
2025-11-17 10:42:24 -0500

    refactor code to follow MVC

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import passport from 'passport';
+import { signUp, login } from '../controllers/authController.js';
+
+const router = express.Router();
+
+router.post('/signUp', signUp);
+router.post('/login', login, passport.authenticate('local', {
+    successRedirect: '/editor',
+    failureRedirect: '/login'
+}));
+
+export default router;
---
+++ routes/pageRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getHomePage, getSignUpPage, getLoginPage, getEditorPage } from '../controllers/pageController.js';
+import { checkAuth } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/', getHomePage);
+router.get('/signUp', getSignUpPage);
+router.get('/login', getLoginPage);
+router.get('/editor', checkAuth, getEditorPage);
+
+export default router;
