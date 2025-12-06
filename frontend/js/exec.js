65f9282b621faf10f6cfed54bed927d528f56125
ConnorSpringer <connorspringer6211@gmail.com>
2025-12-05 20:59:28 -0500

    implemented front-end execution functionality

---
+++ frontend/js/exec.js
@@ -0,0 +1,54 @@
+const outputwindow = document.getElementById("output-text")
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+document.getElementById("execute-btn").addEventListener("click", async () => {
+    try{
+            const fid = document.getElementById("editor").getAttribute("data-file-id");
+        const pid = document.getElementById("editor").getAttribute("data-project-id");
+
+        const tokenres = await fetch('/editor/execute', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                projectId: pid,
+                fileId: fid
+            })
+        }).then(res => {
+            return res.json();
+        });
+
+        const token = tokenres.token;
+        //Poll submission
+        for(let seconds = 0; seconds < 26; seconds++) {
+            outputwindow.textContent = `Waiting for run completion (${26 - seconds} seconds remaining until force stop)`
+            const finishedAt = await fetch(`http://34.46.207.241:2358/submissions/${encodeURIComponent(token)}?base64_encoded=false&fields=finished_at`)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                return data.body.finished_at;
+            });
+
+            if(finishedAt !== null) {
+                const output = await fetch(`http://34.46.207.241:2358/submissions/${encodeURIComponent(token)}?base64_encoded=false&fields=stdout,stderr`)
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    return `Output:\n${data.body.stdout}\nError (If it exists):\n${data.body.stderr}`;
+                });
+                outputwindow.textContent = output;
+                break;
+            }
+            await sleep(1000);
+        }
+    // Yes, this is very general error handling, but it does prevent the page from breaking and gives us a lead for debugging if we need it 
+    } catch(e) {
+        outputwindow.textContent = `An error occured:\n${e}`;
+    }
+})
